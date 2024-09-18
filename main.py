73baeddce5f58ad967de20eee8e60c123c0427e2
medinikrishnan <152921025+medinikrishnan@users.noreply.github.com>
2024-09-17 21:35:40 -0400

    search function implement ENPM611

--- main.py
+++ main.py
@@ -19,13 +19,13 @@ def main():
     # Print JSON data
     print("JSON Data:\n", json_data)
     
-    # uncomment the search_string and search_results once you implement search module
+    
                   # Step 4: Search in JSON
-    # search_string = input("Enter a string to search in the JSON data: ")
-    # search_results = search.search_json(json.loads(json_data), search_string)
+    search_string = input("Enter a string to search in the JSON data: ")
+    search_results = search.search_json(json.loads(json_data), search_string)
     
                   # Print search results
-    # print("Search Results:\n", json.dumps(search_results, indent=4))
+    print("Search Results:\n", json.dumps(search_results, indent=4))
 
 if __name__ == "__main__":
     main()
--- search.py
+++ search.py
@@ -1,8 +1,21 @@
 import json
 
-def search_json(json_data, search_string):
-    results = []
-    # Place your search code here
+# Place your search code here
     # you will have to loop through the json_data file you created earlier
     # finally you can store the match in the result list and return it
+
+def search_json(json_data, search_string):
+    search_string = search_string.lower()
+    results = []
+
+    for item in json_data:
+        #loop thru json_data
+        if search_string in item:
+            #convert value to string & check if it contains search_string
+                results.append({
+                     "User": item.get("User"),
+                     search_string: item.get(search_string)
+                     })
+                #adds to results list if a match is found
+                
     return results
